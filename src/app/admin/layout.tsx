47226fafe2063936efb7974ded348cffb72e0482
conhefarma-del <conhecerfarmacia@gmailcom>
2026-07-09 13:33:46 +0100

    style: move export const dynamic after imports in admin and portal layouts

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import Image from "next/image";
+
+export const dynamic = "force-dynamic";
 import {
   LayoutDashboard,
   MessageSquare,
